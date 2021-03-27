232044fa9d0d6e2ba8dbc81fe9588e58601a2a21
daleksiev <denislav.alexiev00@gmail.com>
2021-03-27 10:20:47 +0200

    Style delete button

--- client/src/components/Products/ProductsDetails/ProductsDetails.js
+++ client/src/components/Products/ProductsDetails/ProductsDetails.js
@@ -48,7 +48,7 @@ const ProductsDetails = ({
                     <strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Deinde qui fit, ut ego nesciam, sciant omnes, quicumque Epicurei esse voluerunt? Qui autem esse poteris, nisi te amor ipse ceperit? Idem iste, inquam, de voluptate quid sentit? Huius ego nunc auctoritatem sequens idem faciam. Maximas vero virtutes iacere omnis necesse est voluptate dominante. Roges enim Aristonem, bonane ei videantur haec: vacuitas doloris, divitiae, valitudo; Unum nescio, quo modo possit, si luxuriosus sit, finitas cupiditates habere. Duo Reges: constructio interrete. Non est igitur voluptas bonum. Atque haec coniunctio confusioque virtutum tamen a philosophis ratione quadam distinguitur.
                 </p>
 
-                <button onClick={onClickDeleteProduct}>Delete Product</button>
+                <button className="product-delete-button" onClick={onClickDeleteProduct}>Delete Product</button>
             </article>
         </section>
     )
