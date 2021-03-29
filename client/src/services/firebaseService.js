973b51f65b2e0960e5007de85f3104f04c76f0fe
daleksiev <denislav.alexiev00@gmail.com>
2021-03-29 22:01:21 +0300

    Update firebaseService.js

--- client/src/services/firebaseService.js
+++ client/src/services/firebaseService.js
@@ -4,13 +4,14 @@ import userService from './userService';
 const signup = (email, password) => {
 	return auth()
 		.createUserWithEmailAndPassword(email, password)
-		.then(({ user }) => userService.create({
-			email,
-			firebaseId: user.uid,
-			refreshToken: user.refreshToken,
-			accessToken: user.za,
-		})
-		);
+		.then(({ user }) => (
+			userService.create({
+				email,
+				firebaseId: user.uid,
+				refreshToken: user.refreshToken,
+				accessToken: user.za,
+			})
+		));
 }
 
 const login = (email, password) => {
