a1ad152008b0701521127dfae98fd95d376602fa
daleksiev <denislav.alexiev00@gmail.com>
2021-04-02 00:44:16 +0300

    Add brand model
    #19

---
+++ server/models/Brand.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const brandSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Brand name is required!'],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image Url is required!'],
+        validate: {
+            validator: (value) => /^https?:\/\/(.*)/.test(value),
+            message: ({ value }) => `${value} is not a valid url address!`,
+        },
+    },
+});
+
+module.exports = mongoose.model('Brand', brandSchema);
