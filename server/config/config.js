eb358eecedc9ec5358c7cccc1f40c97700ef01ed
daleksiev <denislav.alexiev00@gmail.com>
2021-04-04 23:09:19 +0300

    Update package json and config for the server deployment

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 const config = {
     development: {
-        PORT: 5000,
+        PORT: process.env.PORT || 5000,
         dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
         // dbUrl: 'mongodb://localhost/tshop',
     },
     production: {
-        PORT: 5001,
+        PORT: process.env.PORT || 5001,
         dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
     }
 }
