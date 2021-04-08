dc131a2083a0952c7c5364a3398e0302e14ef972
daleksiev <denislav.alexiev00@gmail.com>
2021-04-08 22:01:11 +0300

    Add all user links in the user menu

--- client/src/components/Header/HeaderUserMenu/HeaderUserMenu.js
+++ client/src/components/Header/HeaderUserMenu/HeaderUserMenu.js
@@ -4,6 +4,12 @@ import './HeaderUserMenu.scss';
 const HeaderUserMenu = () => (
     <section className="header-user-menu">
         <Link to="/profile">Profile</Link>
+
+        <Link to="/favourites">Favourites</Link>
+
+        <Link to="/cart">Cart</Link>
+
+        <Link to="/orders">Orders</Link>
     </section>
 )
 
