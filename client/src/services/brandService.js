6c01d49be5781432c45972c7b8a1f350d650ae41
daleksiev <denislav.alexiev00@gmail.com>
2021-04-08 11:52:10 +0300

    Add brand create functionality

--- client/src/services/brandService.js
+++ client/src/services/brandService.js
@@ -3,12 +3,33 @@ import {
     brandUrl,
 } from './api';
 import requester from './requester';
+import { saveImage, deleteImage } from './storageService'
+const uploadImageAndFetch = async (data, token, url, method) => {
+    return saveImage(data.image, 'brands/')
+        .then(imageUrl => method(url, { ...data, image: data.image.name, imageUrl }, { auth: token }));
+}
 
 const getAll = async () => requester.get(brandsUrl());
 const getOne = async (id) => requester.get(brandUrl(id));
-const create = async (data, token) => requester.post(brandsUrl(), data, { auth: token });
-const update = async (id, data, token) => requester.patch(brandUrl(id), data, { auth: token });
-const remove = async (id, token) => requester.delete(brandUrl(id), {}, { auth: token });
+const create = async (data, token) => {
+    if (data?.image?.name) {
+        return uploadImageAndFetch(data, token, brandsUrl(), requester.post)
+    }
+
+    return requester.post(brandsUrl(), data, { auth: token });
+}
+const update = async (id, data, token) => {
+    if (data?.image?.name) {
+        return uploadImageAndFetch(data, token, brandUrl(id), requester.patch)
+    }
+
+    return requester.patch(brandUrl(id), data, { auth: token });
+}
+const remove = async (id, token) => {
+    return getOne(id)
+        .then(brand => deleteImage(brand.image, 'brands/'))
+        .then(() => requester.delete(brandUrl(id), {}, { auth: token }));
+}
 
 const brandService = {
     getAll,
