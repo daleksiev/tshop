ee2756347e9b811646aa99b91cd6a645307dabee
daleksiev <denislav.alexiev00@gmail.com>
2021-04-08 15:07:51 +0300

    Make only the admins to see this page

--- client/src/components/Brands/Brands.js
+++ client/src/components/Brands/Brands.js
@@ -1,19 +1,26 @@
 import { useEffect } from 'react';
 import {
     getBrandsList,
+    getUser,
 } from '../../reducers';
 import { fetchAllBrandsAsync } from '../../actions/brandsActions';
 import BrandsItem from './BrandsItem';
 import { connect } from 'react-redux';
 import './Brands.scss';
+import { Redirect } from 'react-router';
 
 const Brands = ({
     brands,
+    user,
     fetchAllBrandsAsync,
 }) => {
     useEffect(() => {
         fetchAllBrandsAsync()
     }, [fetchAllBrandsAsync])
+
+    if (user?.role !== 'admin') {
+        return <Redirect to="/" />
+    }
 
     return (
         <section className="brands-container">
@@ -24,6 +31,7 @@ const Brands = ({
 
 const mapStateToProps = (state) => ({
     brands: getBrandsList(state),
+    user: getUser(state),
 })
 
 const mapDispatchToProps = {
