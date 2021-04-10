c9434f851da321cc737f93ea8cbd1965f0a5ecd6
daleksiev <denislav.alexiev00@gmail.com>
2021-04-10 13:43:52 +0300

    Add model

---
+++ server/models/Order.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    date: {
+        type: Date,
+        default: Date.now(),
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    products: [{
+        type: Object,
+    }],
+    price: Number,
+});
+
+module.exports = mongoose.model('Order', orderSchema);
