d5a2638565ebbab5f50922d01f2200ba1c69fa91
daleksiev <denislav.alexiev00@gmail.com>
2021-11-07 02:16:18 +0200

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 const config = {
     development: {
-        PORT: process.env.PORT || 80,
+        PORT: process.env.PORT || 5000,
         // dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
         dbUrl: 'mongodb://localhost/tshop',
     },
     production: {
-        PORT: process.env.PORT || 80,
+        PORT: process.env.PORT || 5001,
         dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
     }
 }
