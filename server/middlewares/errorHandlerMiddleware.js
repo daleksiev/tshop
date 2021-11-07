53ed97e6c1a1b328e495f134d79db23ad7aa7ecf
daleksiev <denislav.alexiev00@gmail.com>
2021-11-07 02:34:38 +0200

    Update errorHandlerMiddleware.js

--- server/middlewares/errorHandlerMiddleware.js
+++ server/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-    if (err?.errors) {
+    if (err.errors) {
         let message = '';
 
         for (const key in err.errors) {
@@ -10,7 +10,7 @@ module.exports = (err, req, res, next) => {
         return res.status(400).json({ ok: false, message });
     }
 
-    if (err?.message) {
+    if (err.message) {
         return res.status(400).json({ ok: false, message: err.message });
     }
 
