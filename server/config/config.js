0206184ec18b6586bc995904ca51ce4e23dc2f35
daleksiev <denislav.alexiev00@gmail.com>
2022-12-01 23:23:39 +0200

    Update

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 const config = {
-    development: {
-        PORT: process.env.PORT || 5000,
-        // dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
-        dbUrl: 'mongodb://localhost/tshop',
-    },
-    production: {
-        PORT: process.env.PORT || 5001,
-        dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
-    }
+  development: {
+    PORT: process.env.PORT || 5000,
+    dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
+    // dbUrl: 'mongodb://localhost/tshop',
+  },
+  production: {
+    PORT: process.env.PORT || 5001,
+    dbUrl: 'mongodb+srv://denis:softuni_deni@cluster0.ktyer.mongodb.net/tshop?retryWrites=true&w=majority',
+  }
 }
 
 const env = process.env.NODE_ENV?.trim() || 'development';
